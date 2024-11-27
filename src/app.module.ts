9edbabdce38e7b19d161d350ab711fd176bf342c
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2024-11-27 19:30:37 -0300

    feat(user, prisma): implement user crud functions, configure prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
