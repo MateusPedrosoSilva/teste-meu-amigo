159dc53ea54c4c50d0e2dd881c322c414a099704
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2024-11-28 01:28:57 -0300

    feat(pets): create pets resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DatabaseModule, PetsModule],
   controllers: [],
   providers: [],
 })
